0f92878c4b310c43f0a1ed7835d3bf259ec22df4
glmsr12 <gulumsercakmak@hotmail.com>
2023-01-10 03:11:44 -0800

    "update files"

---
+++ index.js
@@ -0,0 +1,7 @@
+const getMinutes = document.querySelector('.minutes');
+const getSeconds = document.querySelector('.seconds');
+const btnStart = document.querySelector('.btn-start');
+const btnStop = document.querySelector('.btn-stop');
+const btnReset = document.querySelector('.btn-reset');
+
+//start button
